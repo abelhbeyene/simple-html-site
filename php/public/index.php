913bee62a137db2940791d610577c928eb4d7f6b
Abel <contact.abeyene@gmail.com>
2022-06-09 14:21:42 +0100

    Php quiz added

---
+++ php/public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * https://www.php.net/manual/en/curl.examples-basic.php
+ */
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
--- script.js
+++ script.js
@@ -6,6 +6,11 @@ document.getElementById('search-input').addEventListener('keyup', async (e) => {
     // Things to look out for
     // ---
     // Use es6
-    // Error handling
+
+    const res = await fetch('http://localhost:8000/')
+    const json = await res.json()
+
+    const result = `<li>${json.join('</li><li>')}</li>`
+    document.getElementById('results').innerHTML = result
 
 })
